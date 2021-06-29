a3611e25ae183380365d91e9db7c0385f9e896b3
Juan Felix <andrade.juan@outlook.com.br>
2021-06-28 22:10:39 -0300

    :sparkles: Add restart pipeline runtime

--- src/pipeline/client/main.pipeline.ts
+++ src/pipeline/client/main.pipeline.ts
@@ -1,6 +1,7 @@
 import { MessageEvent } from '@/domain/models/message-event'
 import { Pipeline } from '@/domain/models/pipeline'
 import { Whatsapp } from 'venom-bot'
+import { addressPipeline } from './address.pipeline'
 import { confirmOrderPipeline } from './confirm-order.pipeline'
 import { menuPipeline } from './menu.pipeline'
 import { orderPipeline } from './order.pipeline'
@@ -10,14 +11,19 @@ export async function mainPipeline (client: Whatsapp, messageEvent: MessageEvent
   const tasksPipelines = [
     welcomePipeline,
     menuPipeline,
+    addressPipeline,
     orderPipeline,
     confirmOrderPipeline
   ] as const
 
   // menu
   for (const pipeline of tasksPipelines) {
-    const { error, processed }: Pipeline.Result = await pipeline(client, messageEvent)
+    const { error, processed, restartPipeline }: Pipeline.Result = await pipeline(client, messageEvent)
     if (error) throw new Error(error)
+    if (restartPipeline) {
+      mainPipeline(client, messageEvent)
+      break
+    }
     if (processed) break
   }
 }
