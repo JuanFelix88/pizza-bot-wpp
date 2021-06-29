07829c97810713e4c0f42d7a5da2f274fea1e3bb
Juan Felix <andrade.juan@outlook.com.br>
2021-06-28 22:05:49 -0300

    :art: Add implements method in context user

--- src/infra/persistent-context/insert-context-in-user.ts
+++ src/infra/persistent-context/insert-context-in-user.ts
@@ -28,6 +28,31 @@ export class InsertContextInUser {
 
     return context as ContextCache<T>
   }
+
+  public async implementsData<T>
+  (from: ContextCache.From, name: keyof ContextCache.Data, data: T): Promise<ContextCache<T>> {
+    const existentContext = this.contextCacheMap.get(from)
+
+    const context: ContextCache =
+      existentContext
+        ? {
+            name,
+            data: {
+              ...existentContext.data,
+              [name]: { ...existentContext.data[name] || {}, ...data }
+            }
+          }
+        : {
+            name,
+            data: {
+              [name]: data
+            }
+          }
+
+    this.contextCacheMap.set(from, context)
+
+    return context as ContextCache<T>
+  }
 }
 
 export const insertContextInUser = new InsertContextInUser()
