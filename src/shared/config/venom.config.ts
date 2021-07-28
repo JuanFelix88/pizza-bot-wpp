0a9ea4119645d05e3d4531efe1965ce1ba7febd9
Juan Felix <andrade.juan@outlook.com.br>
2021-07-28 14:57:00 -0300

    chore: Edit venom.config to dynamic client

--- src/shared/config/venom.config.ts
+++ src/shared/config/venom.config.ts
@@ -5,11 +5,12 @@ export const config: CreateOptions = {
   folderNameToken: 'tokens', // folder name when saving tokens
   mkdirFolderToken: '', // folder directory tokens, just inside the venom folder, example:  { mkdirFolderToken: '/node_modules', } //will save the tokens folder in the node_modules directory
   headless: true, // Headless chrome
-  useChrome: true, // If false will use Chromium instance
+  useChrome: false, // If false will use Chromium instance
   debug: false, // Opens a debug session
-  logQR: true, // Logs QR automatically in terminal
+  logQR: false, // Logs QR automatically in terminal
   browserWS: '', // If u want to use browserWSEndpoint
   browserArgs: [''], // Parameters to be added into the chrome browser instance
+  waitForLogin: true,
   puppeteerOptions: {
     handleSIGINT: false,
     handleSIGHUP: false,
@@ -21,3 +22,27 @@ export const config: CreateOptions = {
   autoClose: 60000, // Automatically closes the venom-bot only when scanning the QR code (default 60 seconds, if you want to turn it off, assign 0 or false)
   updatesLog: false
 }
+
+export const configWithQrCode = catchQrCode => ({
+  session: 'test',
+  folderNameToken: 'tokens', // folder name when saving tokens
+  mkdirFolderToken: '', // folder directory tokens, just inside the venom folder, example:  { mkdirFolderToken: '/node_modules', } //will save the tokens folder in the node_modules directory
+  headless: true, // Headless chrome
+  catchQR: catchQrCode,
+  useChrome: false, // If false will use Chromium instance
+  debug: false, // Opens a debug session
+  logQR: false, // Logs QR automatically in terminal
+  browserWS: '', // If u want to use browserWSEndpoint
+  browserArgs: [''], // Parameters to be added into the chrome browser instance
+  waitForLogin: true,
+  puppeteerOptions: {
+    handleSIGINT: false,
+    handleSIGHUP: false,
+    handleSIGTERM: false
+
+  }, // Will be passed to puppeteer.launch
+  disableSpins: true, // Will disable Spinnies animation, useful for containers (docker) for a better log
+  disableWelcome: true, // Will disable the welcoming message which appears in the beginning
+  autoClose: 60000, // Automatically closes the venom-bot only when scanning the QR code (default 60 seconds, if you want to turn it off, assign 0 or false)
+  updatesLog: false
+} as CreateOptions)
